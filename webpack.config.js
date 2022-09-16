7cdf630c3e12d036e65744a5b4a0505e3e996435
vipulpathak113 <vipulpathak113@gmail.com>
2022-09-17 02:34:40 +0530

    dev server and loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,28 @@
-const path = require('path')
+const path = require("path");
 
-module.exports={
-    mode:"development",
-    entry: path.resolve(__dirname,'src/index.js'),
-    output:{
-        path: path.resolve(__dirname,'dist'),
-        filename:'bundle.js'
-    }
-}
+module.exports = {
+  mode: "development",
+  entry: path.resolve(__dirname, "src/index.js"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  devServer:{
+    static:{
+        directory: path.resolve(__dirname,'dist')
+    },
+    port:3000,
+    open:true,
+    hot:true,
+    compress:true,
+    historyApiFallback:true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader','css-loader','sass-loader']
+      },
+    ],
+  },
+};
